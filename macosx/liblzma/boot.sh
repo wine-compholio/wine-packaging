8f1b2acda52eb37e63ec8bb7bb8e535ef8eca65f
Sebastian Lackner <sebastian@fds-team.de>
2015-12-27 07:44:51 +0100

    macosx/liblzma: Rename source tarball.

--- macosx/liblzma/boot.sh
+++ macosx/liblzma/boot.sh
@@ -20,12 +20,12 @@ apt-get install -y git devscripts build-essential
 chmod +x /usr/bin/i686-apple-darwin12-dsymutil
 
 {{
-	download("xz.tar.gz", "http://tukaani.org/xz/xz-5.2.2.tar.gz",
+	download("liblzma.tar.gz", "http://tukaani.org/xz/xz-5.2.2.tar.gz",
 		     "73df4d5d34f0468bd57d09f2d8af363e95ed6cc3a4a86129d2f2c366259902a2")
 }}
 
-su builder -c "tar -xvf xz.tar.gz --strip-components 1"
-rm xz.tar.gz
+su builder -c "tar -xvf liblzma.tar.gz --strip-components 1"
+rm liblzma.tar.gz
 
 su builder -c "./configure --prefix=/usr --host i686-apple-darwin12"
 cp /build/source/config.log /build/
