6037cec0d633ddde964428f34e79677c4b1d5d17
Sebastian Lackner <sebastian@fds-team.de>
2015-12-29 23:45:53 +0100

    macosx: Cleanup macosx-common.sh

--- macosx/macosx-common.sh
+++ macosx/macosx-common.sh
@@ -4,11 +4,16 @@ dpkg -i /build/source/deps/cctools-i686-darwin12_877.5-1~jessie_i386.deb \
         /build/source/deps/bomutils_0.2-1~jessie_i386.deb \
         /build/source/deps/xar_1.6.1-1~jessie_i386.deb
 
+# Extract MacOS rootfs
 mkdir /build/macos-rootfs
-cd /build/macos-rootfs
-tar -xf ../source/deps/MacOSX10.8.sdk.tar.xz --strip-components 1
+(cd /build/macos-rootfs; tar -xf ../source/deps/MacOSX10.8.sdk.tar.xz --strip-components 1)
+chown root:builder /build/macos-rootfs
+chmod 0775 /build/macos-rootfs
 
-# Create compiler wrapper / symlinks
+# Create an empty filelist
+echo -n "" > /build/source/deps/filelist.txt
+
+# Create wrapper for clang
 (
   echo "#!/bin/bash"
   echo "clang -target i686-apple-darwin12 -mlinker-version=0.0 -isysroot \"/build/macos-rootfs\" \"\$@\""
@@ -17,6 +22,7 @@ chmod +x /usr/bin/i686-apple-darwin12-clang
 
 ln -s /usr/bin/i686-apple-darwin12-clang /usr/bin/i686-apple-darwin12-gcc
 
+# Create wrapper for clang++
 (
   echo "#!/bin/bash"
   echo "clang++ -target i686-apple-darwin12 -mlinker-version=0.0 -isysroot \"/build/macos-rootfs\" \"\$@\""
@@ -26,7 +32,7 @@ chmod +x /usr/bin/i686-apple-darwin12-clang++
 ln -s /usr/bin/i686-apple-darwin12-clang++ /usr/bin/i686-apple-darwin12-g++
 ln -s /usr/bin/i686-apple-darwin12-clang++ /usr/bin/i686-apple-darwin12-cpp
 
-# Create wrapper for pkg config
+# Create wrapper for pkg-config
 (
   echo "#!/bin/bash"
   echo "export PKG_CONFIG_DIR="
@@ -42,8 +48,3 @@ chmod +x /usr/bin/i686-apple-darwin12-pkg-config
   echo "echo \"dsymutil stub: \$@\" >&2"
 ) > /usr/bin/i686-apple-darwin12-dsymutil
 chmod +x /usr/bin/i686-apple-darwin12-dsymutil
-
-chown root:builder /build/macos-rootfs
-chmod 0775 /build/macos-rootfs
-
-cd /build/source
