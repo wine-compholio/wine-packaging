882a1c4527586c8061dc81fe76efde3a9fb88cb0
Sebastian Lackner <sebastian@fds-team.de>
2015-12-30 21:00:11 +0100

    macosx/wine: Improve code for url encoding.

--- macosx/wine/osx-package/payload-wine/Contents/Resources/start-script.sh
+++ macosx/wine/osx-package/payload-wine/Contents/Resources/start-script.sh
@@ -26,10 +26,11 @@ function _urlencode()
     local length="${#1}"
     for (( i = 0; i < length; i++ )); do
         local c="${1:i:1}"
-        case $c in
+        case "$c" in
             [a-zA-Z0-9.~_-]) printf "$c" ;;
+            " ") printf "+" ;;
             *) printf '%s' "$c" | xxd -p -c1 |
-                   while read c; do printf '%%%s' "$c"; done ;;
+                while read c; do printf '%%%s' "$c"; done ;;
         esac
     done
 }
