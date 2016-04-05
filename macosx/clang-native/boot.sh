bbe86aa9fe077bbff214ad5728f83542805af3bd
Michael Müller <michael@fds-team.de>
2016-04-05 04:04:13 +0200

    macosx: Use clang 3.8 for builds.

--- macosx/clang-native/boot.sh
+++ macosx/clang-native/boot.sh
@@ -8,32 +8,32 @@ apt-get install -y git devscripts build-essential
 
 {{
 	debian_sources = [
-		("llvm-toolchain-3.5_3.5-10.dsc",
-		 "f3229ceeb39479fea20adcabf3e59bf8a850442f287e86302d581a34060f147a"),
-		("llvm-toolchain-3.5_3.5.orig-clang-tools-extra.tar.bz2",
-		 "ea549356548118ef82b727e1051a1b336bf2e8ea60c7932953749efccbd8be7f"),
-		("llvm-toolchain-3.5_3.5.orig-clang.tar.bz2",
-		 "afdd0af66908e7f91c9dc70e51f77fc97626b5fd59b94c3eba464ecdfd3e703a"),
-		("llvm-toolchain-3.5_3.5.orig-compiler-rt.tar.bz2",
-		 "d011491517640a87a4aa060ae9cb3a0de32bc8d3b0c373f7434904d6fad083dc"),
-		("llvm-toolchain-3.5_3.5.orig-lldb.tar.bz2",
-		 "8898513f1ecb05d07140d2377f1006d9ff3d04150025dba0355a0babfdcb5eb8"),
-		("llvm-toolchain-3.5_3.5.orig-polly.tar.bz2",
-		 "e1373b39c76a72f227058765159487dd53858afdf254bff7e881741ea908c1dd"),
-		("llvm-toolchain-3.5_3.5.orig.tar.bz2",
-		 "36649be6cecb54d0cd3d6148fffa42f7d43f53e3f8e0303f35a75ad15d83aec3"),
-		("llvm-toolchain-3.5_3.5-10.debian.tar.xz",
-		 "93af761eaeedb6af56f2d21a2e22aa4eb801ca044c86ca7fd01a4d73aacda51e"),
+		("llvm-toolchain-3.8_3.8-2.dsc",
+		 "4444047c26b50222361af61a11b6eebc05a0744c93fa75af3b0ca1d90204d30f"),
+		("llvm-toolchain-3.8_3.8.orig-clang-tools-extra.tar.bz2",
+		 "829294015ce07d3f115f5dda2422c9c4efbcb0f3d704df9673b0f3ad238ae390"),
+		("llvm-toolchain-3.8_3.8.orig-clang.tar.bz2",
+		 "c9a786040bbda4f2aa7d26474567bf4d9c9b9a0fa5b0f5fea51c6f4f37fe62d1"),
+		("llvm-toolchain-3.8_3.8.orig-compiler-rt.tar.bz2",
+		 "93e34592b651377ed86d6085e1b71cfad8c4023ded934d5f03ca700eb56a888e"),
+		("llvm-toolchain-3.8_3.8.orig-lldb.tar.bz2",
+		 "9664e4f349d22de29fd4eb6945c93995c72a4a19aaa176c31ba592c7d4fcf349"),
+		("llvm-toolchain-3.8_3.8.orig-polly.tar.bz2",
+		 "c0f408b252685dfb15a7e0818305efacbf56190f128f5f08fea36284f7e4327a"),
+		("llvm-toolchain-3.8_3.8.orig.tar.bz2",
+		 "e9f28eef0e452efcf03fea2f24e336c126bd63578c9db21bf1544f326bbd8405"),
+		("llvm-toolchain-3.8_3.8-2.debian.tar.xz",
+		 "8866c9f1a82e475e881bb9992d901287b94d510f1ed67a35a8118cf03b039388"),
 	]
 
 	for name, sha in debian_sources
-		download(name, "http://http.debian.net/debian/pool/main/l/llvm-toolchain-3.5/%s" % name, sha)
+		download(name, "http://http.debian.net/debian/pool/main/l/llvm-toolchain-3.8/%s" % name, sha)
 	endfor
 }}
 
 cd ..
-su builder -c "dpkg-source -x source/llvm-toolchain-3.5_3.5-10.dsc"
-cd llvm-toolchain-3.5-3.5
+su builder -c "dpkg-source -x source/llvm-toolchain-3.8_3.8-2.dsc"
+cd llvm-toolchain-3.8-3.8
 
 if ls ../source/*.patch &> /dev/null; then
 	cat ../source/*.patch | patch -p1
