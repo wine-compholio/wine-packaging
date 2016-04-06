53d627adcb121c8d5e08054145b8673d386c980a
Michael Müller <michael@fds-team.de>
2016-04-06 18:15:03 +0200

    macosx: Create universal binary for liblzma.

--- macosx/liblzma/boot.sh
+++ macosx/liblzma/boot.sh
@@ -18,14 +18,25 @@ apt-get install -y git devscripts build-essential
 	download("liblzma.tar.gz", "%s/xz-%s.tar.gz" % (url, package_version), sha)
 }}
 
-su builder -c "tar -xvf liblzma.tar.gz --strip-components 1"
-rm liblzma.tar.gz
+{{ for (host, arch) in [("i686-apple-darwin12", ""), ("x86_64-apple-darwin12", "64")] }}
 
-su builder -c "./configure --prefix=/usr --host i686-apple-darwin12"
-cp /build/source/config.log /build/
-su builder -c "make"
-su builder -c "mkdir /build/tmp"
-su builder -c "make install DESTDIR=/build/tmp/"
+su builder -c "mkdir build{{ =arch }}"
+cd build{{ =arch }}
+su builder -c "tar -xvf ../liblzma.tar.gz --strip-components 1"
+
+su builder -c "./configure --prefix=/usr --host {{ =host }}"
+cp config.log /build/config{{ =arch }}.log
+su builder -c "make -j3"
+su builder -c "mkdir /build/tmp{{ =arch }}"
+su builder -c "make install DESTDIR=/build/tmp{{ =arch }}/"
+
 # "make install" already copies license files to usr/share/doc/xz
-su builder -c "./fixup-import.py --destdir /build/tmp --verbose"
+su builder -c "../fixup-import.py --destdir /build/tmp{{ =arch }} --verbose"
+
+cd ..
+
+{{ endfor }}
+
 su builder -c "(cd /build/tmp; fakeroot tar -cvzf /build/{{ =output }}-osx.tar.gz .)"
+su builder -c "./make-universal.py --dir32 /build/tmp --dir64 /build/tmp64"
+su builder -c "(cd /build/tmp64; fakeroot tar -cvzf /build/{{ =output }}-osx64.tar.gz .)"
