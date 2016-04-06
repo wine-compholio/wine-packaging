e1bcc5508094e0fac613941370ceb8d4ca0a64ad
Michael Müller <michael@fds-team.de>
2016-04-06 23:05:09 +0200

    macosx: Create universal binary for libtxc-dxtn-s2tc.

--- macosx/libtxc-dxtn-s2tc/boot.sh
+++ macosx/libtxc-dxtn-s2tc/boot.sh
@@ -19,18 +19,28 @@ apt-get install -y git devscripts build-essential
 	download("libtxc-dxtn-s2tc.tar.gz", "%s/%s.tar.gz" % (url, version), sha)
 }}
 
-su builder -c "tar -xvf libtxc-dxtn-s2tc.tar.gz --strip-components 1"
-rm libtxc-dxtn-s2tc.tar.gz
+{{ for (host, arch) in [("i686-apple-darwin12", ""), ("x86_64-apple-darwin12", "64")] }}
 
-su builder -c "cat *.patch | patch -p1"
+su builder -c "mkdir build{{ =arch }}"
+cd build{{ =arch }}
+su builder -c "tar -xvf ../libtxc-dxtn-s2tc.tar.gz --strip-components 1"
+
+su builder -c "cat ../*.patch | patch -p1"
 su builder -c "./autogen.sh"
 
-su builder -c "./configure --prefix=/usr --host i686-apple-darwin12"
-cp /build/source/config.log /build/
-su builder -c "make"
-su builder -c "mkdir /build/tmp"
-su builder -c "make install DESTDIR=/build/tmp/"
-su builder -c "mkdir -p /build/tmp/usr/share/doc/libtxc_dxtn"
-su builder -c "cp -a COPYING /build/tmp/usr/share/doc/libtxc_dxtn"
-su builder -c "./fixup-import.py --destdir /build/tmp --verbose"
+su builder -c "./configure --prefix=/usr --host {{ =host }}"
+cp config.log /build/config{{ =arch }}.log
+su builder -c "make -j3"
+su builder -c "mkdir /build/tmp{{ =arch }}"
+su builder -c "make install DESTDIR=/build/tmp{{ =arch }}/"
+su builder -c "mkdir -p /build/tmp{{ =arch }}/usr/share/doc/libtxc_dxtn"
+su builder -c "cp -a COPYING /build/tmp{{ =arch }}/usr/share/doc/libtxc_dxtn"
+su builder -c "../fixup-import.py --destdir /build/tmp{{ =arch }} --verbose"
+
+cd ..
+
+{{ endfor }}
+
 su builder -c "(cd /build/tmp; fakeroot tar -cvzf /build/{{ =output }}-osx.tar.gz .)"
+su builder -c "./make-universal.py --dir32 /build/tmp --dir64 /build/tmp64"
+su builder -c "(cd /build/tmp64; fakeroot tar -cvzf /build/{{ =output }}-osx64.tar.gz .)"
