8cc23b5abf334b3933e8bba7240abf58f7152835
Michael Müller <michael@fds-team.de>
2016-04-06 18:56:17 +0200

    macosx: Create universal binary for liblcms2.

--- macosx/liblcms2/boot.sh
+++ macosx/liblcms2/boot.sh
@@ -12,8 +12,8 @@ apt-get install -y git devscripts build-essential nasm automake
 
 {{ =include("../macosx-common.sh") }}
 (
-	tar -C /build/macos-rootfs -xvf /build/source/deps/libjpeg-turbo-*-osx.tar.gz
-	tar -C /build/macos-rootfs -xvf /build/source/deps/libtiff-*-osx.tar.gz
+	tar -C /build/macos-rootfs -xvf /build/source/deps/libjpeg-turbo-*-osx64.tar.gz
+	tar -C /build/macos-rootfs -xvf /build/source/deps/libtiff-*-osx64.tar.gz
 ) > /build/source/deps/filelist.txt
 
 {{
@@ -22,15 +22,25 @@ apt-get install -y git devscripts build-essential nasm automake
 	download("liblcms2.tar.gz", "%s/lcms2-%s.tar.gz" % (url, package_version), sha)
 }}
 
-su builder -c "tar -xvf liblcms2.tar.gz --strip-components 1"
-rm liblcms2.tar.gz
+{{ for (host, arch) in [("i686-apple-darwin12", ""), ("x86_64-apple-darwin12", "64")] }}
 
-su builder -c "./configure --prefix=/usr --host i686-apple-darwin12"
-cp /build/source/config.log /build/
-su builder -c "make"
-su builder -c "mkdir /build/tmp"
-su builder -c "make install DESTDIR=/build/tmp/"
-su builder -c "mkdir -p /build/tmp/usr/share/doc/liblcms2"
-su builder -c "cp -a COPYING /build/tmp/usr/share/doc/liblcms2"
-su builder -c "./fixup-import.py --destdir /build/tmp --filelist /build/source/deps/filelist.txt --verbose"
+su builder -c "mkdir build{{ =arch }}"
+cd build{{ =arch }}
+su builder -c "tar -xvf ../liblcms2.tar.gz --strip-components 1"
+
+su builder -c "./configure --prefix=/usr --host {{ =host }}"
+cp config.log /build/config{{ =arch }}.log
+su builder -c "make -j3"
+su builder -c "mkdir /build/tmp{{ =arch }}"
+su builder -c "make install DESTDIR=/build/tmp{{ =arch }}/"
+su builder -c "mkdir -p /build/tmp{{ =arch }}/usr/share/doc/liblcms2"
+su builder -c "cp -a COPYING /build/tmp{{ =arch }}/usr/share/doc/liblcms2"
+su builder -c "../fixup-import.py --destdir /build/tmp{{ =arch }} --filelist /build/source/deps/filelist.txt --verbose"
+
+cd ..
+
+{{ endfor }}
+
 su builder -c "(cd /build/tmp; fakeroot tar -cvzf /build/{{ =output }}-osx.tar.gz .)"
+su builder -c "./make-universal.py --dir32 /build/tmp --dir64 /build/tmp64"
+su builder -c "(cd /build/tmp64; fakeroot tar -cvzf /build/{{ =output }}-osx64.tar.gz .)"
