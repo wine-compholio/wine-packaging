20017b835a307007b2a4218a97d1783dbdf4d73c
Michael Müller <michael@fds-team.de>
2016-04-08 06:49:22 +0200

    macosx: Add initial support for building and packaging 64-bit Wine.

--- macosx/wine/boot.sh
+++ macosx/wine/boot.sh
@@ -56,40 +56,93 @@ cp /build/wine-tools/config.log /build/config-tools.log
 su builder -c "make __tooldeps__ -j3"
 
 # Build for OSX
-su builder -c "mkdir /build/wine-cross"
-cd /build/wine-cross
-su builder -c "../source/configure --prefix=/usr --host i686-apple-darwin12 --with-wine-tools=../wine-tools \
+{{ for (host, arch, extra_flags) in [("i686-apple-darwin12", "", ""), ("x86_64-apple-darwin12", "64", "--enable-win64")] }}
+
+su builder -c "mkdir /build/wine-cross{{ =arch }}"
+cd /build/wine-cross{{ =arch }}
+su builder -c "../source/configure --prefix=/usr --host {{ =host }} --with-wine-tools=../wine-tools \
 				--x-includes=/build/macos-rootfs/opt/X11/include --x-libraries=/build/macos-rootfs/opt/X11/lib \
-				LDFLAGS='-Wl,-rpath,/opt/x11/lib' CFLAGS='-msse2'"
-cp /build/wine-cross/config.log /build/config-cross.log
+				{{ =extra_flags }} LDFLAGS='-Wl,-rpath,/opt/x11/lib' CFLAGS='-msse2'"
+cp config.log /build/config-cross{{ =arch }}.log
 su builder -c "make -j3"
-su builder -c "mkdir /build/tmp"
-su builder -c "make install DESTDIR=/build/tmp/"
-su builder -c "mkdir -p /build/tmp/usr/share/doc/wine"
-su builder -c "cp -a /build/source/{ANNOUNCE,LICENSE,COPYING.*} /build/tmp/usr/share/doc/wine"
-su builder -c "/build/source/fixup-import.py --destdir /build/tmp --filelist /build/source/deps/filelist.txt --verbose"
-su builder -c "(cd /build/tmp; fakeroot tar -cvzf /build/{{ =output }}-osx.tar.gz .)"
+su builder -c "mkdir /build/tmp{{ =arch }}"
+su builder -c "make install DESTDIR=/build/tmp{{ =arch }}/"
+su builder -c "mkdir -p /build/tmp{{ =arch }}/usr/share/doc/wine"
+su builder -c "cp -a /build/source/{ANNOUNCE,LICENSE,COPYING.*} /build/tmp{{ =arch }}/usr/share/doc/wine"
+su builder -c "/build/source/fixup-import.py --destdir /build/tmp{{ =arch }} --filelist /build/source/deps/filelist.txt --verbose"
+su builder -c "/build/source/fixup-import.py --destdir /build/tmp{{ =arch }} --install_name --verbose"
+su builder -c "(cd /build/tmp{{ =arch }}; fakeroot tar -cvzf /build/{{ =output }}-osx{{ =arch }}.tar.gz .)"
 
-# Install dependencies in DESTDIR
-su builder -c "tar -C /build/tmp -xf /build/source/deps/libjpeg-turbo-*-osx.tar.gz"
-su builder -c "tar -C /build/tmp -xf /build/source/deps/liblcms2-*-osx.tar.gz"
-su builder -c "tar -C /build/tmp -xf /build/source/deps/liblzma-*-osx.tar.gz"
-su builder -c "tar -C /build/tmp -xf /build/source/deps/libopenal-soft-*-osx.tar.gz"
-su builder -c "tar -C /build/tmp -xf /build/source/deps/libtiff-*-osx.tar.gz"
-su builder -c "tar -C /build/tmp -xf /build/source/deps/libxml2-*-osx.tar.gz"
-su builder -c "tar -C /build/tmp -xf /build/source/deps/libxslt-*-osx.tar.gz"
+cd ..
+
+{{ endfor }}
+
+# Unpack dependencies
+{{ for arch in ["", "64"] }}
+
+su builder -c "mkdir /build/tmp-deps{{ =arch }}"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/libjpeg-turbo-*-osx{{ =arch }}.tar.gz"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/liblcms2-*-osx{{ =arch }}.tar.gz"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/liblzma-*-osx{{ =arch }}.tar.gz"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/libopenal-soft-*-osx{{ =arch }}.tar.gz"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/libtiff-*-osx{{ =arch }}.tar.gz"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/libxml2-*-osx{{ =arch }}.tar.gz"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/libxslt-*-osx{{ =arch }}.tar.gz"
 {{ if staging }}
-su builder -c "tar -C /build/tmp -xf /build/source/deps/libtxc-dxtn-s2tc-*-osx.tar.gz"
+su builder -c "tar -C /build/tmp-deps{{ =arch }} -xf /build/source/deps/libtxc-dxtn-s2tc-*-osx{{ =arch }}.tar.gz"
 {{ endif }}
-su builder -c "/build/source/fixup-import.py --destdir /build/tmp --install_name --verbose"
-su builder -c "(cd /build/tmp; fakeroot tar -cvzf /build/portable-{{ =output }}-osx.tar.gz .)"
+su builder -c "/build/source/fixup-import.py --destdir /build/tmp-deps{{ =arch }} --install_name --verbose"
 
-# Create payload directory
+{{ endfor }}
+
+# Create 32 bit portable tar
+su builder -c "mkdir /build/tmp-portable32"
+su builder -c "cp -ar /build/tmp/usr /build/tmp-portable32/"
+su builder -c "cp -ar /build/tmp-deps/usr /build/tmp-portable32/"
+su builder -c "(cd /build/tmp-portable32/; fakeroot tar -cvzf /build/portable-{{ =output }}-osx.tar.gz .)"
+rm -rf /build/tmp-portable32
+
+# Move duplicate files to a separate location
+su builder -c "mkdir /build/tmp32"
+su builder -c "/build/source/move-duplicates.py --dir32 /build/tmp --dir64 /build/tmp64 --out /build/tmp32"
+
+# /build/tmp 	- common 32-bit files
+# /build/tmp64 	- 64-bit only files
+# /build/tmp32  - 32-bit only files
+
+# Create 64 bit portable tar
+su builder -c "mkdir /build/tmp-portable64"
+su builder -c "cp -ar /build/tmp/usr /build/tmp-portable64/"
+su builder -c "cp -ar /build/tmp64/usr /build/tmp-portable64/"
+su builder -c "cp -ar /build/tmp-deps64/usr /build/tmp-portable64/"
+su builder -c "(cd /build/tmp-portable64/; fakeroot tar -cvzf /build/portable-{{ =output }}-osx64.tar.gz .)"
+rm -rf /build/tmp-portable64
+
+# Create payload directory for common 32 bit files
 su builder -c "cp -ar /build/source/osx-package/payload-wine /build/tmp-osx-payload"
 su builder -c "cp -ar /build/tmp/usr /build/tmp-osx-payload/Contents/Resources/wine"
 su builder -c "i686-apple-darwin12-clang -msse2 -framework Cocoa \
                -o /build/tmp-osx-payload/Contents/MacOS/wine \
                /build/source/osx-package/wrapper.m"
+
+# Create payload directory for 32 bit only files
+su builder -c "mkdir -p /build/tmp-osx-payload32/Contents/Resources"
+su builder -c "cp -ar /build/tmp32/usr /build/tmp-osx-payload32/Contents/Resources/wine"
+
+# Create payload directory for 64 bit only files
+su builder -c "mkdir -p /build/tmp-osx-payload64/Contents/Resources"
+su builder -c "cp -ar /build/tmp64/usr /build/tmp-osx-payload64/Contents/Resources/wine"
+
+# Create payload directory for dep files
+su builder -c "mkdir -p /build/tmp-osx-payload-deps/Contents/Resources"
+su builder -c "cp -ar /build/tmp-deps64/usr /build/tmp-osx-payload-deps/Contents/Resources/wine"
+
+# Clean up remaining build files
+rm -rf /build/tmp
+rm -rf /build/tmp32
+rm -rf /build/tmp64
+rm -rf /build/tmp-deps
+rm -rf /build/tmp-deps64
 
 # Assemble package
 cd /build/source/osx-package
@@ -104,7 +157,7 @@ su builder -c "./osx-package.py -C /build/tmp-osx-pkg settings \
 				--welcome 'welcome.html' \
 				--conclusion 'conclusion.html' \
 				--architecture i386 x86_64 \
-				--allow-customization false \
+				--allow-customization always \
 				--allow-external-scripts false \
 				--target-any-volume true \
 				--target-user-home true \
@@ -112,6 +165,7 @@ su builder -c "./osx-package.py -C /build/tmp-osx-pkg settings \
 				--script /build/source/osx-package/install-script \
 				--installation-check 'pm_install_check();'"
 
+# pkg - common 32 bit files
 su builder -c "./osx-package.py -C /build/tmp-osx-pkg pkg-add \
 				--identifier org.winehq.{{ =package }} \
 				--version {{ =package_version }} \
@@ -120,11 +174,67 @@ su builder -c "./osx-package.py -C /build/tmp-osx-pkg pkg-add \
 				--scripts /build/source/osx-package/scripts-wine \
 				--preinstall-script preinstall.sh"
 
+# pkg - 32 bit only files
+su builder -c "./osx-package.py -C /build/tmp-osx-pkg pkg-add \
+				--identifier org.winehq.{{ =package }}32 \
+				--version {{ =package_version }} \
+				--install-location '/Applications/{{ =pretty_name }}.app' \
+				--payload /build/tmp-osx-payload32"
+
+# pkg - 64 bit only files
+su builder -c "./osx-package.py -C /build/tmp-osx-pkg pkg-add \
+				--identifier org.winehq.{{ =package }}64 \
+				--version {{ =package_version }} \
+				--install-location '/Applications/{{ =pretty_name }}.app' \
+				--payload /build/tmp-osx-payload64"
+
+# pkg - universal dependencies (32 + 64 bit)
+su builder -c "./osx-package.py -C /build/tmp-osx-pkg pkg-add \
+				--identifier org.winehq.{{ =package }}-deps \
+				--version {{ =package_version }} \
+				--install-location '/Applications/{{ =pretty_name }}.app' \
+				--payload /build/tmp-osx-payload-deps"
+
+# choice - dependencies (required)
+su builder -c "./osx-package.py -C /build/tmp-osx-pkg choice-add \
+				--id choice0 \
+				--title 'Dependencies' \
+				--description 'Third party libraries which are required for Wine.' \
+				--visible true \
+				--enabled false \
+				--selected true \
+				--pkgs org.winehq.{{ =package }}-deps"
+
+# choice - common 32 bit files (required)
 su builder -c "./osx-package.py -C /build/tmp-osx-pkg choice-add \
 				--id choice1 \
-				--title '{{ =pretty_name }}' \
-				--description 'Installs {{ =package }}' \
+				--title '32 bit support' \
+				--description 'Support for running 32 bit applications in Wine.' \
+				--visible true \
+				--enabled false \
+				--selected true \
 				--pkgs org.winehq.{{ =package }}"
+
+# choice - 32 bit only files (xor 64 bit support)
+su builder -c "./osx-package.py -C /build/tmp-osx-pkg choice-add \
+				--id choice2 \
+				--title '32 bit only files' \
+				--description 'Files that are only required for non WOW 64 support.' \
+				--visible true \
+				--enabled false \
+				--start-selected true \
+				--selected \"!choices['choice3'].selected\" \
+				--pkgs org.winehq.{{ =package }}32"
+
+# choice - 64 bit files (64 bit WOW support)
+su builder -c "./osx-package.py -C /build/tmp-osx-pkg choice-add \
+				--id choice3 \
+				--title '64 bit support (experimental)' \
+				--description 'Support for running 64 bit applications in Wine.' \
+				--visible true \
+				--enabled true \
+				--start-selected false \
+				--pkgs org.winehq.{{ =package }}64"
 
 su builder -c "./osx-package.py -C /build/tmp-osx-pkg generate \
 				--output /build/{{ =output }}.pkg"
