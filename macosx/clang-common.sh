3d7482b047fe34aab2cc45e944d552941fd7e0cd
Sebastian Lackner <sebastian@fds-team.de>
2017-03-02 22:46:18 +0100

    macosx: Add workaround for no longer existent gcc5 packages.

--- macosx/clang-common.sh
+++ macosx/clang-common.sh
@@ -20,7 +20,13 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
 #
 
-apt-get install -y libobjc-5-dev libffi-dev binfmt-support libjsoncpp1
+apt-get install -y libobjc4
+
+dpkg -i /build/source/deps/gcc-5-base_5.4.1-4_amd64.deb \
+        /build/source/deps/libgcc-5-dev_5.4.1-4_amd64.deb \
+        /build/source/deps/libobjc-5-dev_5.4.1-4_amd64.deb
+
+apt-get install -y libffi-dev binfmt-support libjsoncpp1
 
 dpkg -i /build/source/deps/libclang-common-3.8-dev_3.8-2~stretch_amd64.deb \
         /build/source/deps/libclang1-3.8_3.8-2~stretch_amd64.deb \
